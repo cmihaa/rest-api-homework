1bba737e1ebbbf581a4e3d35f14cad104465750e
Cociuban Mihaela <cociubanmihaela@Cociubans-MacBook-Pro.local>
2023-05-21 23:38:56 +0300

    RestMovieController API created

---
+++ rest-api-hw/src/main/java/com/example/restapihw/MovieDao.java
@@ -0,0 +1,46 @@
+package com.example.restapihw;
+
+import org.springframework.stereotype.Component;
+
+import java.util.*;
+@Component
+public class MovieDao {
+    private List<MoviePersisted> movies;
+
+    public MovieDao() {
+        this.movies = new ArrayList<>();
+    }
+
+    public Movie createMovie(Movie movie) {
+        this.movies.add(new MoviePersisted(movie.getName(), movie.getGen(), movie.getYear(), movie.getDirector(), new Random().nextLong()));
+        return movie;
+    }
+
+    public Movie getMovieById(long id) {
+        for (MoviePersisted moviePersisted : this.movies) {
+            if (moviePersisted.getId() == id) {
+                return moviePersisted.toMovie();
+            }
+        }
+        return null;
+    }
+
+
+
+
+    public boolean deleteMovie (long id){
+        for(int i=0; i<this.movies.size(); i++) {
+            if(this.movies.get(i).getId()== id){
+                return this.movies.remove(this.movies.get(i));
+            }
+        }
+        return false;
+    }
+
+
+    public List<MoviePersisted> getMovie() {
+       return this.movies;
+    }
+
+
+}
---
+++ rest-api-hw/src/main/java/com/example/restapihw/RestMovieController.java
@@ -0,0 +1,43 @@
+package com.example.restapihw;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+@RequestMapping("/api/v1")
+
+public class RestMovieController {
+    @Autowired
+    private MovieDao movieDao;
+
+    @PostMapping("/movies")
+    public ResponseEntity<Movie> createMovie (@RequestBody Movie m){
+        return ResponseEntity.ok(this.movieDao.createMovie(m));
+    }
+    @GetMapping("/movies/{movieId}")
+    public ResponseEntity<Movie> getMovieById (@PathVariable long movieId){
+        return ResponseEntity.ok(this.movieDao.getMovieById(movieId));
+    }
+
+    @GetMapping("/movies")
+    public ResponseEntity<List<MoviePersisted>> getMovie (){
+        return ResponseEntity.ok(this.movieDao.getMovie());
+    }
+
+    @DeleteMapping("/movies/{movieId}")
+    public ResponseEntity<Boolean> deleteMovie (@PathVariable long movieId) {
+        return ResponseEntity.ok(this.movieDao.deleteMovie(movieId));
+    }
+
+//    @PutMapping("/movies/{movieId}")
+//    public ResponseEntity<Boolean> updateMovie (@PathVariable long movieId) {
+//        return ResponseEntity.ok(this.movieDao.updateMovie(movieId));
+//    }
+
+
+}
